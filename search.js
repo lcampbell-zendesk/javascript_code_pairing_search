90d9ec70fc0e8bc3926ddb229812fb5a54670714
Logan Campbell <lcampbellmcpherson@zendesk.com>
2021-12-02 13:50:01 +1100

    basic skelleton

---
+++ search.js
@@ -0,0 +1,6 @@
+import tickets from "./data/tickets.json";
+import users from "./data/users.json";
+
+export function search() {
+  return "search results";
+}
---
+++ search.test.js
@@ -0,0 +1,5 @@
+import { search } from "./search";
+
+test("returns the string `search results`", () => {
+  expect(search()).toEqual("search results");
+});
